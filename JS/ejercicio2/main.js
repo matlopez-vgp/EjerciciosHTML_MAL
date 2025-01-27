aa4264b9c4a7fc7e8c074e9912873da1dd4e40c9
matlopez-vgp <matheus.lopez16@gmail.com>
2025-01-27 20:48:08 +0100

    commit

---
+++ JS/ejercicio2/main.js
@@ -0,0 +1,8 @@
+const cumpleanos = {
+  dia: 26,
+  mes: "Septiembre",
+};
+
+console.log("Mi cumpleaños es el " + cumpleanos.dia + " de " + cumpleanos.mes);
+
+console.log(`Mi cumpleaños es el ${cumpleanos.dia} de ${cumpleanos.mes}`);
